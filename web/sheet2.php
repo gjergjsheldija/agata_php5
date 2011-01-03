8831f0934a25de33dc6345c217b258b385c15eac
Gjergj Sheldija <gjergj.sheldija@gmail.com>
2011-01-03 23:53:04 +0100

    sheet 2 done

--- web/sheet2.php
+++ web/sheet2.php
@@ -1,79 +1,53 @@
-<table width=800 cellspacing=0 cellpadding=0>
-<tr>
-<td width=84 valign=top>
-    <map name="menu">
-    <area shape="rect" coords="01,01,80,58"    HREF="<?php echo $url1;?>">
-    <area shape="rect" coords="01,64,80,124"   HREF="<?php echo $url2;?>">
-    <area shape="rect" coords="01,126,80,186"  HREF="<?php echo $url3;?>">
-    <area shape="rect" coords="01,188,80,248"  HREF="<?php echo $url4;?>">
+<?php
+// Including the necessary classes and definitions.
+include 'start.php';
+Trans::SetLanguage($lang);
     
-    <area SHAPE="DEFAULT" NOHREF></map>
-    <img src='images/bar2.png' usemap="#menu" ismap border=0><br><br>
-    <center><a href='index.php'><img src='images/browse.png' border=0><br><?php  echo _a('Reports');?></a>
-    </center>
-</td>
-<td width=716 align=left valign=top>
-    <table width=100% cellspacing=0 border=1>
-    <tr>
-    <td valign=top>
-    <table width=100% cellspacing=0 border=0>
-    <tr class=tabletitle height=30>
-    <td colspan=4>
-        <b>&nbsp;Agata CoreReport:: <?php  echo _a('Report Generation'); ?></b>
-    </td>
-    </tr>
-    
-    <tr class=tablepath>
-    <td colspan=4>
-        &nbsp;
-    <?php  echo _a('Report Levels'); ?>
-    
-    </td>
-    </tr>
+$Report = CoreReport::OpenReport($file);
+$Breaks = CoreReport::ExtractBreaks($Report);
+$Elements  = MyExplode(trim($Report['Report']['DataSet']['Query']['Select']), _a('Column'), true);
+
+   ?>
+   <link href="site.css" rel="stylesheet" type="text/css">
+<form>
+
+<h1>Agata CoreReport :: <?php echo _a('Report Generation'); ?></h1>
+<ul id="nav">
+	<li>
+		<a href="sheet1.php?file=<?php echo $_REQUEST['file'];?>">Report</a>
+	</li>
+	<li class="active">
+		<a href="sheet2.php?file=<?php echo $_REQUEST['file'];?>">Grouping</a>
+	</li>
+	<li>
+		<a href="sheet3.php?file=<?php echo $_REQUEST['file'];?>">Graphing</a>
+	</li>
+	<li>
+		<a href="sheet4.php?file=<?php echo $_REQUEST['file'];?>">Merging</a>
+	</li>
+	<li style="float:right">
+		<a href="browse.php?goal=1">Reports</a>
+	</li>	
+</ul>
+
+<h2><?php  echo _a('Report Levels'); ?></h2>
+<fieldset class="settings">   
     <?php 
-        $Report = CoreReport::OpenReport($file);
-        $Breaks = CoreReport::ExtractBreaks($Report);
-        $Elements  = MyExplode(trim($Report['Report']['DataSet']['Query']['Select']), _a('Column'), true);
-        if ($Breaks)
-        {
-            $i = 1;
-            $line = 1;
-            foreach ($Breaks as $break=>$formula)
-            {
-                $line = $line ==1 ? 2: 1;
-                $i ++;
+
+        if ($Breaks) {
+            foreach ($Breaks as $break=>$formula) {
     
-                echo "<tr class=line$line>";
-                echo '<td colspan=4>';
-                echo str_repeat('<blockquote>', $i);
-                echo '<img src=images/ico_level.png>&nbsp;&nbsp;';
-                if ($break == 0)
-                {
-                    echo _a('Level')  . " $break : " . _a('Grand Total') . '<br>';
-                }
-                else
-                {
-                    echo $Elements[$break] . '<br>';
+                if ($break == 0) {
+                    echo "<label>" . _a('Level')  . " $break : " . _a('Grand Total') . '</label>';
+                } else {
+                    echo "<label>" . $Elements[$break]  . '</label>';
                 }
 
                 $Formulas = CoreReport::TranslateFormulas($Report['Report']['DataSet']['Query']['Select'], $formula);
-                foreach ($Formulas as $Formula)
-                {
-                    echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  $Formula<br>";
+                foreach ($Formulas as $Formula) {
+                    echo "<label>$Formula</label>";
                 }
-                echo str_repeat('</blockquote>', $i);
-                echo '</td>';
-                echo '</tr>';
             }
         }
-    ?>
-    </table>
-    </td>
-    <td bgcolor="#8280fe" valign=top width=32 >
-        <img src='images/image.png'>
-    </td>
-    </tr>
-    </table>
-</td>
-</tr>
-</table>
+?>
+</fieldset>
