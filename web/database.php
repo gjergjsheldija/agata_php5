233601af50368fe313df516126438c08d99f3652
Gjergj Sheldija <gjergj.sheldija@gmail.com>
2011-02-09 20:36:25 +0100

    still removing stuff

--- web/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = sqlite_open('agataweb.db');
-sqlite_query($conn, 'create table users (login, name, email, password)');
-sqlite_query($conn, 'create table rights (login, report)');
-
-sqlite_close($conn);
-?>
